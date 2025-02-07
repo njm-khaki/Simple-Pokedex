d3df3fc6e81d733e6bb9d93d10def229ecdb0339
njm-khaki <cornet19970121@gmail.com>
2025-02-07 11:49:57 +0900

    ポケモン詳細画面 複数の姿を表示

--- flutter_demo_mock_app/lib/ui/pokemon_detail/pokemon_detail_page.dart
+++ flutter_demo_mock_app/lib/ui/pokemon_detail/pokemon_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo_mock_app/models/pokemon_detail/pokemon_detail.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,6 +13,17 @@ class PokemonDetailPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final sprites = [
+      pokemon.sprites.frontDefault,
+      pokemon.sprites.frontFemale,
+      pokemon.sprites.frontShiny,
+      pokemon.sprites.frontShinyFemale,
+      pokemon.sprites.backDefault,
+      pokemon.sprites.backFemale,
+      pokemon.sprites.backShiny,
+      pokemon.sprites.backShinyFemale,
+    ].where((url) => url != null).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(pokemon.name),
@@ -21,10 +33,20 @@ class PokemonDetailPage extends ConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(
-            pokemon.sprites.frontDefault ?? "",
-          ),
-          Text(pokemon.name),
+            CarouselSlider.builder(
+              options: CarouselOptions(
+                initialPage: 1,
+                viewportFraction: 1,
+                enableInfiniteScroll: false,
+              ),
+              itemCount: sprites.length,
+              itemBuilder: (context, index, realIndex) {
+                return Image.network(
+                  sprites[index] ?? "",
+                );
+              },
+            ),
+            Text(pokemon.name),
           ],
         ),
       ),
